fd3e4ea43e158a3bd416237cc790d772995f0836
Charles050900 <charlesbellarmin05@gmail.com>
2025-08-26 00:33:50 +0530

    Auto update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,29 +3,86 @@ import Carousel from "react-bootstrap/Carousel"
 import CarouselImage1 from "../assets/carousel-1.jpg"
 import CarouselImage2 from "../assets/carousel-2.jpg"
 import CarouselImage3 from "../assets/carousel-3.jpg"
+import CarouselImage4 from "../assets/carousel-4.jpg"
+import CarouselImage5 from "../assets/carousel-5.jpg"
+import CarouselImage6 from "../assets/carousel-6.jpg"
+import CarouselImage7 from "../assets/carousel-7.jpg"
+import CarouselImage8 from "../assets/carousel-8.jpg"
+import CarouselImage9 from "../assets/carousel-9.jpg"
+import CarouselImage10 from "../assets/carousel-10.jpg"
+import "../CSS/Profile.css"
 
 function CarouselFadeExample() {
     return (
-        <Carousel fade interval={1500}>
+        <Carousel fade>
             <Carousel.Item>
                 <img className="d-block w-100" src={CarouselImage1} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="d-block w-100" src={CarouselImage2} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100 h" src={CarouselImage3} style={{ height: "85vh", objectFit: "cover" }} />
+                <img className="d-block w-100 " src={CarouselImage3} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage4} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage5} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage6} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage7} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage8} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage9} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={CarouselImage10} style={{ height: "85vh", objectFit: "cover" }} />
+                <Carousel.Caption>
+                    <h3 className="carousel-heading">அன்பின் அரவணைப்பு அறக்கட்டளை</h3>
+                    <p className="carousel-content">அனைவருக்கும் அனைத்தும் கிடைக்க வேண்டும், அனைவரும் சம நிலையில் வாழ வேண்டும்.</p>{" "}
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
