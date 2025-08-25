80ef3916494d5ad05f45f4517cc400d37edc0b5a
Charles050900 <charlesbellarmin05@gmail.com>
2025-08-25 22:37:56 +0530

    Auto update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,28 @@
 import Carousel from "react-bootstrap/Carousel"
 // import ExampleCarouselImage from "components/ExampleCarouselImage"
 import CarouselImage1 from "../assets/carousel-1.jpg"
+import CarouselImage2 from "../assets/carousel-2.jpg"
+import CarouselImage3 from "../assets/carousel-3.jpg"
 
 function CarouselFadeExample() {
     return (
-        <Carousel fade>
+        <Carousel fade interval={1500}>
             <Carousel.Item>
-                <img className="d-block w-100" src={CarouselImage1} style={{ height: "90vh", objectFit: "cover" }} />
+                <img className="d-block w-100" src={CarouselImage1} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" src={CarouselImage1} style={{ height: "90vh", objectFit: "cover" }} />
+                <img className="d-block w-100" src={CarouselImage2} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img className="d-block w-100" src={CarouselImage1} style={{ height: "90vh", objectFit: "cover" }} />
+                <img className="d-block w-100 h" src={CarouselImage3} style={{ height: "85vh", objectFit: "cover" }} />
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
