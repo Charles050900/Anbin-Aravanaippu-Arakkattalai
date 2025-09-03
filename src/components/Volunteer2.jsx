e063d2385c9d68db04dd8e737bb15c520152787e
Charles050900 <charlesbellarmin05@gmail.com>
2025-09-03 16:44:53 +0530

    Auto update

--- src/components/Volunteer2.jsx
+++ src/components/Volunteer2.jsx
@@ -11,21 +11,21 @@ const Volunteer2 = () => {
                     {/* Top Advisory */}
                     <div className="card p-4 mb-5 shadow rounded text-center bg-primary text-white box">
                         <h4 className="mb-1 fw-bold">Dr. V. Murugan</h4>
-                        <p className="mb-0">Advisory of Board & Editor/Publisher – Nigalkalam News</p>
+                        <p className="mb-0">Advisory of Board & Editor/Publisher - Nigalkalam News</p>
                     </div>
                 </div>
             </div>
 
             {/* Advisory Members */}
             <h4 className="text-center mb-4 fw-bold">Advisory Members</h4>
-            <div className="row mb-5">
-                <div className="col-lg-6 mb-3 d-lg-flex justify-content-end">
-                    <div className="card p-3 w-50 shadow rounded text-center bg-primary text-white">
+            <div className="row mb-5 ">
+                <div className="col-lg-6 mb-3 d-flex justify-content-lg-end justify-content-center  ">
+                    <div className="card p-3 w-50 shadow rounded text-center bg-primary text-white ">
                         <h5 className="mb-1 fw-bold">M. Deepa, MBA</h5>
                         <p className="mb-0">Advisory of Board</p>
                     </div>
                 </div>
-                <div className="col-md-6 mb-3 d-lg-flex justify-content-start">
+                <div className="col-md-6 mb-3 d-flex justify-content-lg-start justify-content-center">
                     <div className="card p-3 w-50 shadow rounded text-center bg-primary text-white">
                         <h5 className="mb-1 fw-bold">V. Abisha, MBA</h5>
                         <p className="mb-0">Advisory of Board</p>
@@ -36,25 +36,25 @@ const Volunteer2 = () => {
             {/* Board Members */}
             <h4 className="text-center mb-4 fw-bold">Board Members</h4>
             <div className="row">
-                <div className="col-md-6 mb-4 d-lg-flex justify-content-end">
+                <div className="col-md-6 mb-4 d-flex justify-content-lg-end justify-content-center">
                     <div className="card p-3 w-50 shadow rounded text-center">
                         <h5 className="mb-1 fw-bold">M. Jabadurai, B.A., B.L., B.Ed</h5>
                         <p className="mb-0">Member of Board / Advocate</p>
                     </div>
                 </div>
-                <div className="col-md-6 mb-4 d-lg-flex justify-content-start">
+                <div className="col-md-6 mb-4  d-flex justify-content-lg-start justify-content-center">
                     <div className="card p-3 w-50 shadow rounded text-center">
                         <h5 className="mb-1 fw-bold">M. Kathirvel</h5>
                         <p className="mb-0">Member of Board / Govt. Doc Registrar</p>
                     </div>
                 </div>
-                <div className="col-md-6 mb-3 d-lg-flex justify-content-end">
+                <div className="col-md-6 mb-3 d-flex justify-content-lg-end justify-content-center">
                     <div className="card p-3 w-50 shadow rounded text-center">
                         <h5 className="mb-1 fw-bold">K. Kamali, B.Sc.</h5>
                         <p className="mb-0">Member of Board</p>
                     </div>
                 </div>
-                <div className="col-md-6 mb-3 d-lg-flex justify-content-start">
+                <div className="col-md-6 mb-3 d-flex justify-content-lg-start justify-content-center">
                     <div className="card p-3 w-50 shadow rounded text-center">
                         <h5 className="mb-1 fw-bold">D. Preethi, MBA</h5>
                         <p className="mb-0">Member of Board</p>
