ebc6074494e5ee1f5cb5a3c2f1f4b278faac8456
Charles050900 <charlesbellarmin05@gmail.com>
2025-09-24 22:23:33 +0530

    Auto update

--- src/components/Volunteer1.jsx
+++ src/components/Volunteer1.jsx
@@ -38,7 +38,7 @@ const Volunteer1 = () => {
                 </div>
             </div>
 
-            {/* <!-- Treasurers --> */}
+            {/* <!-- Trustees --> */}
             <div className="row justify-content-center">
                 <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center">
                     <div className="org-connector-y rounded-5"></div>
@@ -71,7 +71,7 @@ const Volunteer1 = () => {
                     {/* <div className="org-connector"></div> */}
                 </div>
 
-                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center">
+                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center mt-5 mt-md-0">
                     <div className="org-box">
                         {/* <img src={pic} alt="" className="img-fluid w-50 rounded-4 mb-4" /> */}
                         <h5 className="fw-bold">G. Thenral Nilavan</h5>
--- src/components/VolunteerRegistrationProfile.jsx
+++ src/components/VolunteerRegistrationProfile.jsx
@@ -4,7 +4,7 @@ import "../CSS/VolunteerRegistrationProfile.css"
 const VolunteerRegistrationProfile = () => {
     return (
         <div className="volunteer-register-profile p-5 d-flex flex-column justify-content-end align-items-center">
-            <h2 className="fw-bold text-white mb-4 register-title">Hands Together, Hearts Together</h2>
+            <h2 className="fw-bold text-white text-center mb-4 register-title">Hands Together, Hearts Together</h2>
             <h5 className="fw-bold text-white text-center register-content">Lend your hands, share your heart, Together we can make a fresh start</h5>
         </div>
     )
