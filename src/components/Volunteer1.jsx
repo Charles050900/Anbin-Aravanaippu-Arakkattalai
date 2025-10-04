868f6264ceabbe78b0d0904ddb2ef3ee87c45d89
Charles050900 <charlesbellarmin05@gmail.com>
2025-10-04 15:44:53 +0530

    Auto update

--- src/components/Volunteer1.jsx
+++ src/components/Volunteer1.jsx
@@ -10,7 +10,7 @@ const Volunteer1 = () => {
                 <img src={title} alt="" className="" />
             </div>
             {/* <!-- B. BOMBAY --> */}
-            <div className="row justify-content-center mb-5">
+            <div className="row justify-content-center ">
                 <div className="col-10 col-sm-8 col-md-6 col-lg-3 text-center">
                     {/* <h2 className="mb-5 fw-bold text-white">Organisers</h2> */}
                     <div className="org-box vol-shadow">
@@ -18,57 +18,57 @@ const Volunteer1 = () => {
                         <h5 className="fw-bold ">B. Bombay</h5>
                         <p className="mb-0">Founder & President of the Trust</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
+                    <div className="org-connector"></div>
                 </div>
             </div>
 
             {/* <!-- B. KAWASKAR --> */}
-            <div className="row justify-content-center mb-5">
+            <div className="row justify-content-center ">
                 <div className="col-10 col-sm-8 col-md-6 col-lg-3 text-center">
                     <div className="org-box vol-shadow">
                         {/* <img src={pic} alt="" className="img-fluid w-50 rounded-4 mb-4" /> */}
                         <h5 className="fw-bold">B. Kawaskar</h5>
                         <p className="mb-0">General Secretary of the Trust</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
-                    <div className="row">
-                        <div className="col-lg-2"></div>
-                        {/* <div className="org-connector-x col-lg-8 rounded "></div> */}
+                    <div className="org-connector"></div>
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-lg-0"></div>
+                        <div className="org-connector-x col-lg-8 d-none d-md-block col-1 rounded   "></div>
                     </div>
                 </div>
             </div>
 
             {/* <!-- Trustees --> */}
             <div className="row justify-content-center">
-                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center mb-4">
-                    {/* <div className="org-connector-y rounded-5"></div> */}
+                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center ">
+                    <div className="org-connector-y rounded-5 d-none d-md-block"></div>
                     <div className="org-box vol-shadow">
                         {/* <img src={pic} alt="" className="img-fluid w-50 rounded-4 mb-4" /> */}
                         <h5 className="fw-bold">K. Kamali</h5>
                         <p className="mb-0">Trustee</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
+                    <div className="org-connector d-none d-md-block"></div>
                 </div>
 
-                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center mb-4">
-                    {/* <div className="org-connector-y rounded-5"></div> */}
+                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center">
+                    <div className="org-connector-y rounded-5"></div>
                     <div className="org-box vol-shadow">
                         {/* <img src={pic} alt="" className="img-fluid w-50 rounded-4 mb-4" /> */}
                         <h5 className="fw-bold">B. Kabildev</h5>
                         <p className="mb-0">Trustee</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
+                    <div className="org-connector"></div>
                 </div>
             </div>
 
             <div className="row justify-content-center ">
-                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center mb-4 ">
+                <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center ">
                     <div className="org-box vol-shadow">
                         {/* <img src={pic} alt="" className="img-fluid w-50 rounded-4 mb-4" /> */}
                         <h5 className="fw-bold">D. Kanishkar</h5>
                         <p className="mb-0">Trustee</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
+                    <div className="org-connector d-md-none"></div>
                 </div>
 
                 <div className="col-10 col-sm-5 col-md-4 col-lg-2 text-center  mt-md-0">
@@ -77,7 +77,6 @@ const Volunteer1 = () => {
                         <h5 className="fw-bold">G. Thenral Nilavan</h5>
                         <p className="mb-0">Trustee</p>
                     </div>
-                    {/* <div className="org-connector"></div> */}
                 </div>
             </div>
         </div>
